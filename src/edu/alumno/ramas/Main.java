2ca81e3b20057108ebfa89a3b9ed207f29b6ba95
eespqui0601 <eespqui0601@g.educaand.es>
2025-11-20 11:11:14 +0100

    Mensaje en consola (rama feature/mensaje)

--- src/edu/alumno/ramas/Main.java
+++ src/edu/alumno/ramas/Main.java
@@ -17,5 +17,7 @@ public class Main {
         
         int suma = bolas.stream().mapToInt(Integer::intValue).sum();
         System.out.println("Suma de principales: " + suma);
+        
+        System.out.println("¡Suerte en el sorteo, DAM!");
     }
 }
